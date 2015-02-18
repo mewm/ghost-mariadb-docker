b017bdd7a29936d59f24005b48080b25355511da
Dennis Micky Jensen <root@mewm.org>
2015-02-18 09:28:51 +0100

    Cleaned a bit up

--- mariadb/create-mariadb-user.sh
+++ mariadb/create-mariadb-user.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 /usr/bin/mysqld_safe > /dev/null 2>&1 &
 
 RET=1
--- mariadb/run-mariadb.sh
+++ mariadb/run-mariadb.sh
@@ -2,13 +2,13 @@
 
 VOLUME_HOME="/var/lib/mysql"
 if find ${VOLUME_HOME} -maxdepth 0 -empty | read v; then
-    echo "=> An empty or uninitialized MariaDB volume is detected in $VOLUME_HOME"
-    echo "=> Installing MariaDB ..."
+    echo " -> Installation detected in $VOLUME_HOME"
+    echo " -> Installing MariaDB"
     mysql_install_db > /dev/null 2>&1
-    echo "=> Done!"
-    /create_mariadb_admin_user.sh
+    echo " -> Done!"
+    /create-mariadb-admin-user.sh
 else
-    echo "=> Using an existing volume of MariaDB"
+    echo "-> Booting on existing volume!"
 fi
 
 exec mysqld_safe
