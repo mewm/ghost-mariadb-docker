06b0e522788bc401e5c3f026a8cb1ee4a4caa17f
Dennis Micky Jensen <root@mewm.org>
2015-03-08 22:27:07 +0100

    Better new relic support

--- ghost/install_newrelic.sh
+++ ghost/install_newrelic.sh
@@ -6,5 +6,5 @@ cp node_modules/newrelic/newrelic.js newrelic.js
 sed -i -r "s/My Application/mewm blog/" newrelic.js
 sed -i -r "s/info/trace/" newrelic.js
 sed -i -r "s/license key here/${1}/" newrelic.js
-sed -i "1ivar newrelic = require('newrelic');" core/index.js
+sed -i "1ivar newrelic = require('newrelic');" index.js
 
