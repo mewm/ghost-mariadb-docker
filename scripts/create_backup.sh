1194c939d70ef4ec333208a012ba9d2303540eef
Dennis Micky Jensen <root@mewm.org>
2015-03-08 21:49:13 +0100

    Better path support in backup scripts

--- scripts/create_backup.sh
+++ scripts/create_backup.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
+
+script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd );
+
 if [ "$1" = "db" ]; then
 	echo "Creating database backup ${2}"
-	docker run --volumes-from ghostmariadbdocker_data_1 -v "/Users/mewm/www/ghost-mariadb-docker/backups":/backups ubuntu tar cvf /backups/$2 /var/lib/mysql
+	docker run --volumes-from ghostmariadbdocker_data_1 -v ${script_dir}/../backups:/backups ubuntu tar cvf /backups/$2 /var/lib/mysql
 elif [ "$1" = "ghost" ]; then
 	echo "Creating ghost backup ${2}"
-	docker run --volumes-from ghostmariadbdocker_data_1 -v "/Users/mewm/www/ghost-mariadb-docker/backups":/backups ubuntu tar cvf /backups/$2 /var/www/ghost/content 
+	docker run --volumes-from ghostmariadbdocker_data_1 -v ${script_dir}/../backups:/backups ubuntu tar cvf /backups/$2 /var/www/ghost/content 
 else
 	echo "Usage: sh create_bacup.sh db|ghost file.tar"
 fi
--- scripts/restore_backup.sh
+++ scripts/restore_backup.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
+
+script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd );
+
 if [ "$1" = "db" ]; then
 	echo "Restoring database backup ${2}"
-	docker run --volumes-from ghostmariadbfig_data_1 -v $(pwd)/../backups:/backups ubuntu tar xvf /backups/$2
+	docker run --volumes-from ghostmariadbdocker_data_1 -v ${script_dir}/../backups:/backups ubuntu tar xvf /backups/$2
 elif [ "$1" = "ghost" ]; then
 	echo "Restoring ghost backup ${2}"
-	docker run --volumes-from ghostmariadbfig_data_1 -v $(pwd)/../backups:/backups ubuntu tar xvf /backups/$2
+	docker run --volumes-from ghostmariadbdocker_data_1 -v ${script_dir}/../backups:/backups ubuntu tar xvf /backups/$2
 else
 	echo "Usage: sh restore_bacup.sh db|ghost file.tar"
 fi
